a1ee0971e1e66013c5b50ac07330154ae80bb455
Wiliam <wlrorschach@gmail.com>
2019-07-04 20:22:25 -0300

    Testes de Update, Delete

--- jdbc3/src/application/Program.java
+++ jdbc3/src/application/Program.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 import db.DB;
@@ -27,15 +28,36 @@ public class Program {
 					+ "INSERT INTO seller"
 					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
 					+ "VALUES"
-					+ "(?, ?, ?, ?, ?)");
+					+ "(?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS); // retorna o id gerado
 	
-			st.setString(1, "Carl Purple");
-			st.setString(2, "carl@gmail.com");
+			st.setString(1, "Sherkock Holmes");
+			st.setString(2, "holmes@gmail.com");
+			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1898").getTime()));
+			st.setDouble(4, 3000.0);
+			st.setInt(5, 4);
 			
-			//st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
+			int rowsAffected = st.executeUpdate();
+				ResultSet rs = st.getGeneratedKeys();
+				
+				while (rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("Done! Id = " + id);
+				}
+				
+			if (rowsAffected > 0) {
+				
+			} else {
+				System.out.println("No rows affected!");
+			}
+			
 			
 		} catch (SQLException e) {
 			
+		} catch (ParseException e) {
+			e.printStackTrace();
+		} finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
 		}
 	
 	}
